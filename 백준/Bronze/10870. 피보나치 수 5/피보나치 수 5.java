0eeab00f737284cecc20d76c201db78b83aec146
이승희 <seungheelee1122@gmail.com>
2026-01-08 10:15:46 +0900

    Update 피보나치 수 5.java

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -2,13 +2,12 @@
 0
 1
 a[i] = a[i-2] + a[i-1]
-i는 2부터 n까지.
-i++
-만약 4라면 i는 2부터 4까지
+i는 2부터 n까지. i++
 피보나치는 빨리 크게 커지니깐 long
-자바 배열 선언 구조는 long[] a = new long[n+1]
+자바 배열 선언 구조는 long[] a = new long[n+1] (필요한 배열의 크기만큼)
 */
 
+/*
 import java.io.*;
 import java.util.*;
 
@@ -26,6 +25,36 @@ public class Main{
             }
         }
         pw.println(a[n]);
+        br.close();
+        pw.close();
+    }
+}
+*/
+
+// 재귀함수로 문제를 풀기
+/*
+a[i-1]이랑 a[i-2]를 더한 값을 결과값으로 return 하는 함수.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static long fibo(int n){
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        return fibo(n-1) + fibo(n-2);   
+    }
+    
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+        int n = Integer.parseInt(br.readLine());
+        pw.println(fibo(n));
         br.close();
         pw.close();
     }
