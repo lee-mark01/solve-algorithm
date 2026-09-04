039cb8b468a8411c64bade55ebe91a5567a7b4ca
이승희 <seungheelee1122@gmail.com>
2026-09-04 15:47:16 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.13 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        return answer = array[array.length / 2];
+    }
+}
+// 정렬하고 길이의 /2 + 1인덱스의 값이잖아.
